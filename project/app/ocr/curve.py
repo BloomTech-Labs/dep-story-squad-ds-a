ab08618134be748f9b1e50b436353060859ba443
jesse berkowitz <jberkow@brandeis.edu>
2020-10-15 19:31:42 -0400

    tweek functions, started adding ELO_Score functionality

--- project/app/ocr/curve.py
+++ project/app/ocr/curve.py
@@ -504,59 +504,71 @@ def Pipeline(Database_list):
     
     return Finalmatchups     
 
+def Elo(listofdicts):
+     
+    Matchups = matchmaker(listofdicts)
+    Player_List = []
+    for player in Matchups.keys():
+        Player_List.append(player)
+
+    ELO_Score = [1000] *len(Player_List)
+    Ratings_Dict =  dict(zip(Player_List, ELO_Score)) 
+
+    return Ratings_Dict
+
 if __name__ == "__main__":
     
-    # string = "Great success. My name is Borat. I have come to America, to find Pamela Anderson, and \
-    #     make her my wife. Very nice!"
-    # string2 = "take in a database of URLs associated with particular usernames, store it in dictionary with scores,\
-    # append it to dict_list2, now should have list of dictionaries to scroll through"
-    # string3 = " Once you have a list of dictionaries, you can scroll through each score related to each function, \
-    # one by one,  and get the data needed to start implementing the curve function, to then ultimately, \
-    #     return star values for each player"
-    # string4 = "Remember that the Learning Rate is a hyperparameter that is specific to your gradient-descent based optimizer \
-    # selection. A learning rate that is too high will cause divergent behavior, but a Learning Rate that is\
-    #      too low will fail to converge, again, you're looking for the sweet spot."
-    # string5 = "Momentum is a hyperparameter that is more commonly associated with Stochastic Gradient Descent. \
-    # SGD is a common optimizer because it's what people understand and know, but I doubt it will get you the \
-    #     best results, you can try hyperparameter tuning its attributes and see if you can beat the performance from adam."         
-    # string6 = "Using dropout on hidden layers might not have any effect while using dropout on hidden layers might\
-    #  have a substantial effect. You don't necessarily need to turn use dropout unless you see that your model\
-    #       has overfitting and generalizability problems."
-    # string7 = "In the case of a binomial outcome (flipping a coin), the binomial distribution may be \
-    # approximated by a normal distribution (for sufficiently large n {\displaystyle n} n). Because \
-    #     the square of a standard normal distribution is the chi-square distribution with one degree of freedom,\
-    #          the probability of a result such as 1 heads in 10 trials can be approximated either by using \
-    #              the normal distribution directly, or the chi-square distribution for the normalised,\
-    #                   squared difference between observed and expected value."
-    # string8 = "The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly\
-    #  that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well. "
-    # string9 = "Tell me that first, and then, if I like being that person,\
-    #      I’ll come up: if not, I’ll stay down here till I’m somebody else’—but, \
-    #          oh dear!” cried Alice, with a sudden burst of tears, “I do wish they\
-    #               would put their heads down! I am so very tired of being all alone here"
+    string = "Great success. My name is Borat. I have come to America, to find Pamela Anderson, and \
+        make her my wife. Very nice!"
+    string2 = "take in a database of URLs associated with particular usernames, store it in dictionary with scores,\
+    append it to dict_list2, now should have list of dictionaries to scroll through"
+    string3 = " Once you have a list of dictionaries, you can scroll through each score related to each function, \
+    one by one,  and get the data needed to start implementing the curve function, to then ultimately, \
+        return star values for each player"
+    string4 = "Remember that the Learning Rate is a hyperparameter that is specific to your gradient-descent based optimizer \
+    selection. A learning rate that is too high will cause divergent behavior, but a Learning Rate that is\
+         too low will fail to converge, again, you're looking for the sweet spot."
+    string5 = "Momentum is a hyperparameter that is more commonly associated with Stochastic Gradient Descent. \
+    SGD is a common optimizer because it's what people understand and know, but I doubt it will get you the \
+        best results, you can try hyperparameter tuning its attributes and see if you can beat the performance from adam."         
+    string6 = "Using dropout on hidden layers might not have any effect while using dropout on hidden layers might\
+     have a substantial effect. You don't necessarily need to turn use dropout unless you see that your model\
+          has overfitting and generalizability problems."
+    string7 = "In the case of a binomial outcome (flipping a coin), the binomial distribution may be \
+    approximated by a normal distribution (for sufficiently large n {\displaystyle n} n). Because \
+        the square of a standard normal distribution is the chi-square distribution with one degree of freedom,\
+             the probability of a result such as 1 heads in 10 trials can be approximated either by using \
+                 the normal distribution directly, or the chi-square distribution for the normalised,\
+                      squared difference between observed and expected value."
+    string8 = "The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly\
+     that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well. "
+    string9 = "Tell me that first, and then, if I like being that person,\
+         I’ll come up: if not, I’ll stay down here till I’m somebody else’—but, \
+             oh dear!” cried Alice, with a sudden burst of tears, “I do wish they\
+                  would put their heads down! I am so very tired of being all alone here"
 
-    # a = store(string, "bill")
-    # b = store(string2, "Kate")
-    # c = store(string3, "Edward")
-    # d = store(string4, "Bobby")
-    # e = store(string5, "Hadi")
-    # f = store(string6, "Jesse")
-    # g = store(string7, "Pierre")
-    # h = store(string8, "Bruce")
-    # i = store(string9, "Franklin")
+    a = store(string, "bill")
+    b = store(string2, "Kate")
+    c = store(string3, "Edward")
+    d = store(string4, "Bobby")
+    e = store(string5, "Hadi")
+    f = store(string6, "Jesse")
+    g = store(string7, "Pierre")
+    h = store(string8, "Bruce")
+    i = store(string9, "Franklin")
 
     
 
-    # dictlist2 = []
-    # dictlist2.append(a)
-    # dictlist2.append(b)
-    # dictlist2.append(c)
-    # dictlist2.append(d)
-    # dictlist2.append(e)
-    # dictlist2.append(f)
-    # dictlist2.append(g)
-    # dictlist2.append(h)
-    # dictlist2.append(i)
+    dictlist2 = []
+    dictlist2.append(a)
+    dictlist2.append(b)
+    dictlist2.append(c)
+    dictlist2.append(d)
+    dictlist2.append(e)
+    dictlist2.append(f)
+    dictlist2.append(g)
+    dictlist2.append(h)
+    dictlist2.append(i)
     database = [
         {
             "user_id": "12322187",
@@ -689,7 +701,7 @@ if __name__ == "__main__":
         # }
     ]
     #actual_dict = [{5206: {'evaluate': 0.386864850604889, 'good_vocab': 0.5025906735751295, 'efficiency': 0.766497461928934, 'decriptiveness': 0.696969696969697, 'sentence_length': 1, 'word_length': 0.4}}, {5229: {'evaluate': 0.37310469710272165, 'good_vocab': 0.3805970149253731, 'efficiency': 0.6323529411764706, 'decriptiveness': 0.6375, 'sentence_length': 1, 'word_length': 0.7}}, {5210: {'evaluate': 0.35023670205895274, 'good_vocab': 0.33986928104575165, 'efficiency': 0.6521739130434783, 'decriptiveness': 0.6704545454545454, 'sentence_length': 1, 'word_length': 0.5}}, {5225: {'evaluate': 0.3042697172108937, 'good_vocab': 0.3247863247863248, 'efficiency': 0.5126050420168067, 'decriptiveness': 0.4805194805194805, 'sentence_length': 1, 'word_length': 0.4}}]
-    #print(Final_Match(actual_dict))
+    #print(Final_Match(dictlist2))
     
     #print(Pipeline(actual_dictionary))
     #abc = Scoredatabase(actual_dictionary)
@@ -697,11 +709,18 @@ if __name__ == "__main__":
     #print(std_dict(dictlist2))
     #a = Scoredatabase(actual_dictionary)
     #print(matchmaker(a))
-    print(Pipeline(actual_dictionary))
+    #print(Pipeline(actual_dictionary))
     #print(dictlist2)
     #print(Scoredatabase(database))
    
-    
+    Matchups = matchmaker(dictlist2)
+    Player_List = []
+    for player in Matchups.keys():
+        Player_List.append(player)
+
+    ELO_Score = [1000] *len(Player_List)
+    Ratings_Dict =  dict(zip(Player_List, ELO_Score)) 
+    print(Ratings_Dict)   
     
     
     
