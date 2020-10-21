fb670c549a22c2e72e524c1693a02988b55e81c1
hadi <modareshadi@gmail.com>
2020-10-21 12:14:39 -0700

    Added token check dependency for get_s3_obj/ endpoint

--- project/app/api/s3.py
+++ project/app/api/s3.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import pandas as pd
 import plotly.express as px
 import boto3
 from starlette.responses import StreamingResponse
 import io
 from pydantic import BaseModel, Field
+from app.dependencies.security import verify_token
+
 
 router = APIRouter()
 
@@ -18,7 +20,7 @@ class S3(BaseModel):
     s3_obj: str = Field(..., example="Stories Dataset/Transcribed Stories/32--/3203/Photo 3203.jpg")
 
 
-@router.post('/get_s3_obj/', tags=["S3 Bucket"])
+@router.post('/get_s3_obj/', tags=["S3 Bucket"], dependencies=[Depends(verify_token)])
 async def s3(params: S3):
     """
     Retreives an s3 jpg image and streams it back.
